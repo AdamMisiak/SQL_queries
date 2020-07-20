e8d1302e7af6204da5cd37a467758fd72f77f919
AdamMisiak <adammisiak3@gmail.com>
2020-07-20 18:35:02 +0200

    INSERT task added

---
+++ INSERT_task.sql
@@ -0,0 +1,10 @@
+CREATE TABLE links(
+            id SERIAL PRIMARY KEY,
+            url VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            description VARCHAR(255),
+            last_update DATE
+);
+
+INSERT INTO links (url, name)
+VALUES('https://www.postgresqltutorial.com','PostgreSQL Tutorial');
